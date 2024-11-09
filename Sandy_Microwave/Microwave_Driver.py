1a8ddf8219988aa2de5258e801f2d30c0884f187
Sandy <sandykerr20@gmail.com>
2024-11-09 12:57:52 -0500

    wrote some important values to csv

--- Sandy_Microwave/Microwave_Driver.py
+++ Sandy_Microwave/Microwave_Driver.py
@@ -1,7 +1,7 @@
 from loader import Microwave_Loader
 
 # load the microwave data and iterate thru it
-loader = Microwave_Loader(day=1, month=6, year=2023, interval=1, end_date=(6, 19))
+loader = Microwave_Loader(day=1, month=1, year=2023, interval=1, end_date=(1, 31))
 loader.iterate_data()
 
 
--- Sandy_Microwave/loader.py
+++ Sandy_Microwave/loader.py
@@ -11,32 +11,25 @@ import numpy as np
 
 # not needed for now
 import os
-import functools
-import math
-
-
+import pandas as pd
 
 class Microwave_Loader:
     '''
         This class allows us to load microwave data beginning on a certain date.
         TODO: see plot colorbars, range of values is a bit strange atm
-        TODO: check for missing values (see email and slack)
     '''
 
     NUM_DAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     
     def __init__(self, month: int = 1, day: int = 1, year: int = 2023, interval: int = 1, end_date: tuple[int, int] = (12, 31)) -> None:
         # raise error on invalid value, otherwise initialize class variables    
-        try:
-            self.check_params(month, day, year, interval, end_date)
-        except(ValueError):
-            pass
+        self.check_params(month, day, year, interval, end_date)
+        # init class instance vars
         self.month = month
         self.day = day
         self.year = year
         self.interval = interval
         self.end_date = end_date
-
     
     def check_params(self, month: int, day: int, year: int, interval: int, end_date: tuple[int, int]) -> bool:
         '''
@@ -64,33 +57,39 @@ class Microwave_Loader:
         else: 
             to_return = True
         return to_return  
-
     
-    def iterate_data(self) -> None:
+    def iterate_data(self, plot_data: bool = True) -> None:
         '''
             Main function of the loader, starts on our initialized date and goes until end_date
             Takes steps of [interval] days with each iteration, plots data and saves to PDF
         '''
-        # year iterator increments date and returns a string date to be used in filename
         year_iter = self.Year_Iterator(self.month, self.day, self.interval, self.end_date)
         filename_middle = str(year_iter)
-        print(f"initial formatted date: {filename_middle}")
-        pdf = PdfPages("Microwave_Plots.pdf")
+       
+        # load a dataframe which we use to write some important values, write the column names to CSV
+        df = pd.DataFrame(columns=["Date", "Missing", "NaN", "Hot_Temps", "Cold_Temps"])
+        self.write_to_csv(df, False, True)
+        # pdf = PdfPages("Microwave_Plots.pdf")
 
         # do while loop: loads & plots at least 1 day of data
         while(True):
+            # get our h5py dataset from formatted date filename, convert to numpy
             dataset_filename = self.get_dataset_filename(filename_middle)
-            print(f"Dataset filename: {dataset_filename}")
             dataset = self.get_dataset(dataset_filename)
-            self.plot_data_single(dataset, *year_iter.get_date_tuple(), pdf)
+
+            # each dataframe is a row of important values that we write to a csv
+            df = self.get_important_values(dataset, df, (*year_iter.get_date_tuple(), self.year))
+            self.write_to_csv(df)
             
             # once we are done iterating, stop the loop and save plots to pdf
             if (not(year_iter.has_next())):
-                pdf.close()
+                # pdf.close()
                 break
             
             filename_middle = next(year_iter)
-    
+        
+        print(f"Final df\n: {df.to_string()}")
+        # self.write_to_csv(df)
    
     def get_dataset(self, filename: str) -> np.array:
         '''
@@ -108,38 +107,39 @@ class Microwave_Loader:
         filename_base = f"{os.getcwd()}\\mw_lst_{str(self.year)}\\MW_LST_DTC_{str(self.year)}"
         filename_end = "_x1y.h5"
         return f"{filename_base}{filename_middle}{filename_end}"
-
-   
-    def plot_data_double(self, dataset: np.array, month: int, day: int, pdf: PdfPages) -> plt.figure:
-        '''
-            Plots our data from 2 evenly-spaced time slices (morning, evening)
-
-            :param dataset: microwave dataset as np array
-            :returns: pyplot (matplotlib) object representing the two plotted time slices
-
-            TODO: make it so that we don't just have hardcoded times of day
-        '''
-        # right now I only support 2 plots per day, evenly spaced
-        fig, (ax1, ax2) = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(12, 12), dpi=80)
-        plt.subplots_adjust(wspace=0.3)
-        
-        # start of day plot
-        ax1.imshow(dataset[24].T)
-        ax1.set_title(f"Morning on: {month}/{day}")
-        ax1.set_xlabel("Latitude")
-        ax1.set_ylabel("Longitude")
-        # plt.colorbar(label="Microwave Temp", orientation="horizontal") 
-        
-        # end of day plot
-        ax2.imshow(dataset[72].T)
-        ax2.set_title(f"Evening on: {month}/{day}")
-        ax2.set_xlabel("Latitude")
-        ax2.set_ylabel("Longitude")
-        # plt.colorbar(label="Microwave Temp", orientation="horizontal") 
-
-        pdf.savefig()
-        return fig
     
+    def get_important_values(self, dataset: np.array, df: pd.DataFrame, date: tuple[int, int, int]) -> dict:
+        '''
+            Testing function to see data that falls within a certain range
+            Appends found values to a pandas dataframe, csv will be saved after iteration
+
+            Dict element 1: number of missing values (-9999 is what Chris said)
+            Dict element 2: number of NaN values
+            Dict element 3: number of values above 20,000 (colorbars say these values exist, but I don't see them)
+            Dict element 4: number of values below 5,000 (plots show a lot of these)
+        '''
+        new_df = pd.DataFrame({
+            "Date": self.format_date_tuple(date),
+            "Missing": [np.count_nonzero(dataset == -9999)],
+            "NaN": [np.count_nonzero(np.isnan(dataset))],
+            "Hot_Temps": [np.count_nonzero(dataset >= 20000)],
+            "Cold_Temps": [np.count_nonzero(dataset <= 2500)]
+        })
+        return new_df
+    
+    def write_to_csv(self, df: pd.DataFrame, header_msg: bool = False, column_labels: bool = False) -> None:
+        '''
+            Writes our dataframe to CSV with optional message, header labels
+            TODO: fix the CSV header, doesn't print correctly
+        '''
+        mode = "w" if column_labels else "a"
+        with open("Microwave_Values.csv", "a") as file:
+            if (header_msg): 
+                start_date_str = self.format_date_tuple((self.month, self.day, self.year))
+                end_date_str = self.format_date_tuple((self.end_date[0], self.end_date[1], self.year))
+                file.write(f"Notable values starting on: {start_date_str} and ending on: {end_date_str}")
+            df.to_csv("Microwave_Values.csv", sep="\t", mode=mode, index=False, encoding='utf-8', header=column_labels)
+        file.close()
     
     def plot_data_single(self, dataset: np.array, month: int, day: int, pdf: PdfPages) -> plt.figure:
         '''
@@ -153,7 +153,12 @@ class Microwave_Loader:
         plt.colorbar(orientation="vertical", mappable=plot)
         pdf.savefig()
 
-    
+    def format_date_tuple(self, date: tuple[int, int, int]) -> str:
+        '''
+            MM/dd/yyyy expected
+        '''
+        return f"{date[0]}/{date[1]}/{date[2]}"
+  
     class Year_Iterator:
         '''
             Handles iteration through days of the year. 
@@ -169,7 +174,6 @@ class Microwave_Loader:
 
             # month, day represents our stopping date
             self.end_date = end_date
-
         
         def __str__(self) -> str:
             '''
