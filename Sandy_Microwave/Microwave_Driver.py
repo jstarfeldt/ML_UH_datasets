37a81794ac87e7b4775e2bee5f94468a46b49f0a
Jon Starfeldt <99223595+jstarfeldt@users.noreply.github.com>
2025-10-03 16:47:15 -0400

    Delete Sandy_Microwave directory

--- Sandy_Microwave/Microwave_Driver.py
+++
@@ -1,7 +0,0 @@
-from loader import Microwave_Loader
-
-# load the microwave data and iterate thru it
-loader = Microwave_Loader(day=1, month=1, year=2023, interval=1, end_date=(1, 31))
-loader.iterate_data()
-
-
